78f05f5f82c71ea6d2a19c3e8506c47533e63729
rmaphoh <rmaphoh@ucl.ac.uk>
2021-07-09 12:27:11 +0100

    20210709

---
+++ test_customise_drive.sh
@@ -0,0 +1,22 @@
+#This is SH file for LearningAIM, testing on customise data
+date
+
+export PYTHONPATH=.:$PYTHONPATH
+
+seed_number=42
+dataset_name='ALL-AV'
+test_checkpoint=1401
+
+CUDA_VISIBLE_DEVICES=0 python test_customise.py --batch-size=1 \
+                                                --dataset=${dataset_name} \
+                                                --job_name=20210628_${dataset_name}_randomseed_${seed_number} \
+                                                --checkstart=${test_checkpoint} \
+                                                --uniform=True \
+                                                --customise_data='./customise_image/' \
+                                                --customise_output='./customise_output/'
+
+date
+
+
+
+
